6cbd535b81f89f064b0815b581b9c31746774507
Poesero <martinferreira09@hotmail.com>
2024-10-30 20:21:32 -0300

    controllers added

---
+++ src/main/java/com/quiendamas/biddingapp/repository/ILoteRepository.java
@@ -0,0 +1,10 @@
+package com.quiendamas.biddingapp.repository;
+
+import com.quiendamas.biddingapp.model.Lote;
+
+import java.util.List;
+
+public interface ILoteRepository {
+
+    public List<Lote> traerTodos();
+}
--- src/main/java/com/quiendamas/biddingapp/repository/LoteRepository.java
+++ src/main/java/com/quiendamas/biddingapp/repository/LoteRepository.java
@@ -1,9 +1,16 @@
 package com.quiendamas.biddingapp.repository;
 
+import com.quiendamas.biddingapp.model.Lote;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public class LoteRepository {
+public class LoteRepository implements ILoteRepository {
+    @Override
+    public List<Lote> traerTodos() {
+        return List.of();
+    }
 
     //métodos para llamar a la base de datos, etc
 }
