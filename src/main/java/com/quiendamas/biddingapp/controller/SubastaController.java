eec625659f6605f222844e8fc23cbf8d21ff73fd
Poesero <martinferreira09@hotmail.com>
2024-11-02 19:26:04 -0300

    @Postmapping for Subasta and Usuario

--- src/main/java/com/quiendamas/biddingapp/controller/SubastaController.java
+++ src/main/java/com/quiendamas/biddingapp/controller/SubastaController.java
@@ -1,7 +1,20 @@
 package com.quiendamas.biddingapp.controller;
 
+import com.quiendamas.biddingapp.model.Subasta;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SubastaController {
+
+    @PostMapping("/subasta")
+    public void newSubasta(@RequestBody Subasta sub){
+        System.out.println(("Subasta cargada con datos: -Nombre:" + sub.getName()
+                + "-Usuario:" + sub.getUserName()
+                + "-Descripción:" + sub.getDescription()
+                + "-Oferta inicial: " + sub.getStartingBid()
+                + "-Duración: " + sub.getDuration()));
+    }
+
 }
--- src/main/java/com/quiendamas/biddingapp/controller/UsuarioController.java
+++ src/main/java/com/quiendamas/biddingapp/controller/UsuarioController.java
@@ -1,7 +1,20 @@
 package com.quiendamas.biddingapp.controller;
 
+import com.quiendamas.biddingapp.model.Usuario;
+import com.quiendamas.biddingapp.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsuarioController {
+
+    @Autowired
+    UsuarioRepository repository;
+
+    @PostMapping("/usuario")
+    public void newUsuario(@RequestBody Usuario user){
+        System.out.println(("Datos del usuario:  -Nombre:" + user.getUserName() + "-Mail:" + user.getMail() ));
+    }
 }
